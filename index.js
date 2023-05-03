c46c5139e0b2851eff8bf556be6cb63c68cd1fe2
levi9111 <captainlevi9111@gmail.com>
2023-05-03 19:02:57 +0600

    chefs id selected

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5300;
 
+const chefs = require("./data/chefs.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = +req.params.id;
+  console.log(id);
+  const selectedChef = chefs.chefs.find((n) => n.id === id);
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
